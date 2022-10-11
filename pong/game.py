762fc90d668b0abc5a13e680c84892cff3e6fe0e
Storres91 <santiagotorres1234.stg@gmail.com>
2022-10-11 10:59:26 -0500

    Refactor code

--- pong/game.py
+++ pong/game.py
@@ -14,14 +14,7 @@ class GameInformation:
 
 
 class Game:
-    """
-    To use this class simply initialize and instance and call the .loop() method
-    inside of a pygame event loop (i.e while loop). Inside of your event loop
-    you can call the .draw() and .move_paddle() methods according to your use case.
-    Use the information returned from .loop() to determine when to end the game by calling
-    .reset().
-    """
-    SCORE_FONT = pygame.font.SysFont("comicsans", 50)
+    SCORE_FONT = pygame.font.SysFont("comicsans", 35)
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     RED = (255, 0, 0)
@@ -44,26 +37,14 @@ class Game:
 
     def _draw_score(self):
         left_score_text = self.SCORE_FONT.render(
-            f"{self.left_score}", 1, self.WHITE)
+            f"Player {self.left_score}", 1, self.WHITE)
         right_score_text = self.SCORE_FONT.render(
-            f"{self.right_score}", 1, self.WHITE)
+            f"AI {self.right_score}", 1, self.WHITE)
         self.window.blit(left_score_text, (self.window_width //
                                            4 - left_score_text.get_width()//2, 20))
         self.window.blit(right_score_text, (self.window_width * (3/4) -
                                             right_score_text.get_width()//2, 20))
 
-    def _draw_hits(self):
-        hits_text = self.SCORE_FONT.render(
-            f"{self.left_hits + self.right_hits}", 1, self.RED)
-        self.window.blit(hits_text, (self.window_width //
-                                     2 - hits_text.get_width()//2, 10))
-
-    def _draw_divider(self):
-        for i in range(10, self.window_height, self.window_height//20):
-            if i % 2 == 1:
-                continue
-            pygame.draw.rect(
-                self.window, self.BLACK, (self.window_width//2 - 5, i, 10, self.window_height//20))
 
     def _handle_collision(self):
         ball = self.ball
@@ -102,13 +83,8 @@ class Game:
     def draw(self, draw_score=True, draw_hits=False):
         self.window.fill(self.BLACK)
 
-        self._draw_divider()
-
         if draw_score:
             self._draw_score()
-
-        if draw_hits:
-            self._draw_hits()
 
         for paddle in [self.left_paddle, self.right_paddle]:
             paddle.draw(self.window)
@@ -116,13 +92,6 @@ class Game:
         self.ball.draw(self.window)
 
     def move_paddle(self, left=True, up=True):
-        """
-        Move the left or right paddle.
-
-        :returns: boolean indicating if paddle movement is valid. 
-                  Movement is invalid if it causes paddle to go 
-                  off the screen
-        """
         if left:
             if up and self.left_paddle.y - Paddle.VEL < 0:
                 return False
@@ -139,12 +108,6 @@ class Game:
         return True
 
     def loop(self):
-        """
-        Executes a single game loop.
-
-        :returns: GameInformation instance stating score 
-                  and hits of each paddle.
-        """
         self.ball.move()
         self._handle_collision()
 
@@ -161,7 +124,6 @@ class Game:
         return game_info
 
     def reset(self):
-        """Resets the entire game."""
         self.ball.reset()
         self.left_paddle.reset()
         self.right_paddle.reset()
